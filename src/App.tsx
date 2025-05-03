84ce001c1aa514e874f3ba1dede481fca278a007
XIAOSHUAI-Y <2138733925@qq.com>
2025-05-03 20:49:50 +0800

    项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Editor from './component/editor/editor'
+
+function App() {
+  return (
+    <>
+      <Editor/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/editor/editor.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './editor.module.scss'
+
+const Editor: React.FC = () => {
+  return (
+    <div className={style['editor-wrapper']}>editor</div>
+  )
+}
+
+export default Editor
